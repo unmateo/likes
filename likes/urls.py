05a16f7ac939a9814adbe77dafda63abd034b262
Mateo Harfuch <mateo.harfuch@onapsis.com>
2022-01-15 17:18:12 -0300

    modular css + basic topnav

--- likes/urls.py
+++ likes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ItemListView, TagListView
+from .views import HomeView, ItemListView, TagListView
 
 app_name = 'likes'
 urlpatterns = [
-    path('tags/', TagListView.as_view()),
-    path('items/', ItemListView.as_view()),
+    path('tags/', TagListView.as_view(), name="tags"),
+    path('items/', ItemListView.as_view(), name="items"),
+    path('', HomeView.as_view(), name="home"),
 ]
--- likes/views.py
+++ likes/views.py
@@ -3,6 +3,11 @@ from django.views import generic
 from .models import Item, Tag
 
 
+
+class HomeView(generic.TemplateView):
+    template_name = "likes/pages/home.html"
+
+
 class ItemListView(generic.ListView):
     model = Item
     template_name = "likes/pages/items.html"
